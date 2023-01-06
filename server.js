5e8010fe258cdbbfde2686f0455bf4706a95c88c
Mayuri811 <pawarmayuri0811@gmail.com>
2023-01-06 11:02:28 +0530

    created routes

--- server.js
+++ server.js
@@ -2,9 +2,44 @@ const http = require("http");
 
 const port = 8081;
 http.createServer((request, response) =>{
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello</h1>");
-    response.end()
+    const {method, url} = request;
+    if(url=== "/todoList")
+    {
+        if(method==="GET")
+        {
+            response.writeHead(200, {"Content-Type": "text/html"});
+            response.write("<h1>Hello</h1>");
+            response.end();
+        }
+        else if(method==="POST")
+        {
+            let body="";
+            request.on('error', (err)=>{
+                console.error(err);
+            })
+            .on('data', (chunk)=>{
+                body+=chunk;
+            })
+            .on('end', ()=>
+            {
+                body=JSON.parse(body);
+                console.log(body);
+            })
+
+
+
+        }
+        else{
+            response.writeHead(501);
+            response.end();
+
+        }
+    }
+    else 
+    {
+        response.writeHead(401)
+        response.end();
+    }
 
 })
 .listen(port, () =>
